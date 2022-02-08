819bacf5921eb8c1a1c701663265b102f561dfb8
Awonke Mnotoza <mnotozawonke@gmail.com>
2022-02-08 16:33:51 +0200

    Links Update

--- book-site/src/components/Header.jsx
+++ book-site/src/components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
               <div className="main-heading">Need a book?</div>
               <h3 className="call-to-action">Buy now</h3>
             </div>
-            <a href="https://api.whatsapp.com/send?phone=636672687&text=I%20would%20like%20to%20inquire%20about%20a%20book%20I%20saw%20on%20the%20site.%20(Please%20type%20the%20book%20name)" className="request">
+            <a href="https://api.whatsapp.com/send?phone=27636672687&text=I%20would%20like%20to%20inquire%20about%20a%20book%20I%20saw%20on%20the%20site.%20(Please%20type%20the%20book%20name)" className="request">
               <p className="request-message">Request a book</p>
               <i class="fab fa-whatsapp"></i>
             </a>
